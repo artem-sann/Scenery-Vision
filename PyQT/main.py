235a2fb16fe0bbe7b976702b37d970b41cc3d213
Artem Sannikov <artemksannikov@gmail.com>
2022-10-25 01:00:23 +0300

    Read exel

--- PyQT/main.py
+++ PyQT/main.py
@@ -1,1 +1,13 @@
-print("Я начал писать код хых")
+import pandas as pd
+
+file = input("Введите название файла: ")
+xl = pd.ExcelFile(file)
+
+# массив из названий листов
+sheet = xl.sheet_names
+
+
+df1 = xl.parse(sheet[0])
+print(df1)
+
+
