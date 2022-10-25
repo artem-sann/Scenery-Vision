502e4318c633e866de9b92cc8408f0b155c51c77
Artem Sannikov <artemksannikov@gmail.com>
2022-10-25 19:03:57 +0300

    Read exel and delete trash

--- PyQT/main.py
+++ PyQT/main.py
@@ -1,13 +1,22 @@
 import pandas as pd
 
-file = input("Введите название файла: ")
+#file = input("Введите название файла: ")
+file = 'All.xlsx'
 xl = pd.ExcelFile(file)
 
-# массив из названий листов
+# берем первый лист и переводим в dataframe
 sheet = xl.sheet_names
-
-# переводим в dataframe
 df1 = xl.parse(sheet[0])
-print(df1)
+
+# удаление пустых столбцов и строчек, сброс индексации и переименование столбцов
+df1.dropna(axis='columns', how='all', inplace=True)
+df1.dropna(axis=0, how='all', inplace=True)
+df1.reset_index(drop=True, inplace=True)
+headers = df1.iloc[0]
+table = pd.DataFrame(df1.values[1:], columns=headers)
+
+
+print(table)
+print(table.iloc[0:3, 0:3])
 
 
