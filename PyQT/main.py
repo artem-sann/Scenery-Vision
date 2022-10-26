a03d499105681708f2ce94d927617d7f64800c94
Artem Sannikov <artemksannikov@gmail.com>
2022-10-26 03:31:22 +0300

    Read exel and refactor table

--- PyQT/main.py
+++ PyQT/main.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 
 #file = input("Введите название файла: ")
@@ -14,9 +15,22 @@ df1.dropna(axis=0, how='all', inplace=True)
 df1.reset_index(drop=True, inplace=True)
 headers = df1.iloc[0]
 table = pd.DataFrame(df1.values[1:], columns=headers)
+table.dropna(axis='columns', how='all', inplace=True)
 
 
-print(table)
+cols = table.columns
+for i in range(len(table.columns)):
+    unics = table[cols[i]].unique()
+    if len(unics) == 1 and table[cols[i]].isna().sum() == 0:
+        table.drop([cols[i]], axis=1, inplace=True)
+
+
+table["JSONВставки"] = table["JSONВставки"].apply(json.loads)
+table["JSONГабариты"] = table["JSONГабариты"].apply(json.loads)
+table["JSONТеги"] = table["JSONТеги"].apply(json.loads)
+
+
 print(table.iloc[0:3, 0:3])
+#print(table.iloc[0, 56])
 
 
