37238b7698524fa513eeabaf0dd77afbb1425926
Artem Sannikov <artemksannikov@gmail.com>
2022-10-28 02:22:45 +0300

    Delete Trash

--- PyQT/main.py
+++ PyQT/main.py
@@ -11,6 +11,32 @@ sheet = xl.sheet_names
 df1 = xl.parse(sheet[0])
 
 
+def delete_empty_info(data):
+    to_del = []
+    data1 = data
+    u = 0
+    for txt in data:
+        for txt1 in txt:
+            if txt[txt1] == "":
+                to_del.append(txt1)
+
+        for p in range(len(to_del)):
+            del data1[u][to_del[p]]
+        to_del.clear()
+        u = u + 1
+
+    return data1
+
+
+def delete_useless_info(data):
+    j = 0
+    for txt in data:
+        if txt["Свойство"] == "ИД товара на площадке Tmall" or txt["Свойство"] == "Код ролика на YouTube":
+            del data[j]
+        j = j + 1
+    return data
+
+
 def filter_camel_case(text):
     if "JSON" in text:
         return text
@@ -43,7 +69,7 @@ headers = df1.iloc[0]
 table = pd.DataFrame(df1.values[1:], columns=headers)
 table.dropna(axis='columns', how='all', inplace=True)
 
-# удаление столбцов где все значения одинаковые
+# удаление столбцов, где все значения одинаковые
 cols = table.columns
 for i in range(len(table.columns)):
     unics = table[cols[i]].unique()
@@ -60,4 +86,9 @@ table["JSONВставки"] = table["JSONВставки"].apply(json.loads)
 table["JSONГабариты"] = table["JSONГабариты"].apply(json.loads)
 table["JSONТеги"] = table["JSONТеги"].apply(json.loads)
 
+# очистка json от мусора
+table["JSONГабариты"] = table["JSONГабариты"].apply(delete_useless_info)
+table["JSONВставки"] = table["JSONВставки"].apply(delete_empty_info)
+
+print(table["JSONВставки"][1])
 print(table)
