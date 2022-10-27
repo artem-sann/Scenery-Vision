f1ca0c30be187ab89c2fb8d562273de2297eea97
Artem Sannikov <artemksannikov@gmail.com>
2022-10-27 19:54:49 +0300

    Delete text between parens

--- PyQT/main.py
+++ PyQT/main.py
@@ -1,13 +1,22 @@
 import json
 import pandas as pd
+import re
 
-#file = input("Введите название файла: ")
+# file = input("Введите название файла: ")
 file = 'All.xlsx'
 xl = pd.ExcelFile(file)
 
 # берем первый лист и переводим в dataframe
 sheet = xl.sheet_names
 df1 = xl.parse(sheet[0])
+
+
+def remove_text_between_parens(text):
+    n = 1
+    while n:
+        text, n = re.subn(r'\([^()]*\)', '', text)
+    return text
+
 
 # удаление пустых столбцов и строчек, сброс индексации и переименование столбцов
 df1.dropna(axis='columns', how='all', inplace=True)
@@ -17,7 +26,6 @@ headers = df1.iloc[0]
 table = pd.DataFrame(df1.values[1:], columns=headers)
 table.dropna(axis='columns', how='all', inplace=True)
 
-
 cols = table.columns
 for i in range(len(table.columns)):
     unics = table[cols[i]].unique()
@@ -25,12 +33,18 @@ for i in range(len(table.columns)):
         table.drop([cols[i]], axis=1, inplace=True)
 
 
+table["JSONГабариты"] = table["JSONГабариты"].apply(remove_text_between_parens)
+table["JSONВставки"] = table["JSONВставки"].apply(remove_text_between_parens)
+table["JSONТеги"] = table["JSONТеги"].apply(remove_text_between_parens)
+
+
+
+
+
 table["JSONВставки"] = table["JSONВставки"].apply(json.loads)
 table["JSONГабариты"] = table["JSONГабариты"].apply(json.loads)
 table["JSONТеги"] = table["JSONТеги"].apply(json.loads)
 
 
-print(table.iloc[0:3, 0:3])
-#print(table.iloc[0, 56])
 
-
+# print(table.iloc[0, 56])
